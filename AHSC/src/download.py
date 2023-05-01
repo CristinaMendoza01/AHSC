2da551efc555184a5f62da85fb2138aa39490ddc
cmendoza <72572198+CristinaMendoza01@users.noreply.github.com>
2023-05-01 22:31:33 +0200

    Update download

--- AHSC/src/download.py
+++ AHSC/src/download.py
@@ -1,5 +1,7 @@
 import json
 import os
+import shutil
+
 import freesound as fs
 
 descriptors = [ 'lowlevel.spectral_centroid.mean',
@@ -10,7 +12,7 @@ descriptors = [ 'lowlevel.spectral_centroid.mean',
                 'sfx.logattacktime.mean',
                 'sfx.inharmonicity.mean']
 
-def download_sounds_freesound(query, directory, api_key, topNresults, featureExt='.json'):
+def download_sounds_freesound(query, tag, duration, directory, api_key, topNresults, featureExt='.json'):
     if query == "":
         print("\n")
         print("Provide a query text to search for sounds")
@@ -33,72 +35,96 @@ def download_sounds_freesound(query, directory, api_key, topNresults, featureExt
     client = fs.FreesoundClient()
     client.set_token(api_key, "token")
 
+    # Creating a duration filter string that the Freesound API understands
+    if duration and type(duration) == tuple:
+        flt_dur = " duration:[" + str(duration[0]) + " TO " + str(duration[1]) + "]"
+    else:
+        flt_dur = ""
+
+    if tag and type(tag) == str:
+        flt_tag = "tag:" + tag
+    else:
+        flt_tag = ""
+
     # Busca sonidos por el texto de búsqueda
     # Querying Freesound
     page_size = 30
-    results = client.text_search(query=query, fields="id,name,previews", page_size=page_size)
+    if not flt_tag + flt_dur == "":
+        results = client.text_search(query=query, filter=flt_tag + flt_dur, sort="score",
+                                  fields="id,name,previews,username,url,analysis", descriptors=','.join(descriptors),
+                                  page_size=page_size, normalized=1)
+
+    else:
+        results = client.text_search(query=query, sort="score", fields="id,name,previews,username,url,analysis",
+                                  descriptors=','.join(descriptors), page_size=page_size, normalized=1)
+    #results = client.text_search(query=query, fields="id,name,previews", page_size=page_size)
 
     # Crea el directorio si aún no existe
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    dir_path = os.path.join(directory, query)
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
+    dir_path = os.path.join(directory + '/' + query)
+    if os.path.exists(dir_path):
+        print(dir_path)
+        print(os.listdir(dir_path))
+        shutil.rmtree(dir_path)
+    os.mkdir(dir_path)
 
-    # Descarga y guarda cada sonido en el directorio
-    for sound in results:
-        sound.retrieve_preview(dir_path, sound.name)
-        print(f"Saving {sound.name} in {dir_path}")
+    pageNo = 1
+    sndCnt = 0
+    indCnt = 0
+    totalSnds = min(results.count, 200)  # System quits after trying to download after 200 times
+    downloadedSounds = []
 
-    mp3Path = os.path.join(directory, str(sound.previews.preview_lq_mp3.split("/")[-1]))
-    print(mp3Path)
-    ftrPath = mp3Path.replace('.mp3', featureExt)
-    print(ftrPath)
-    # Creating directories to store output and downloading sounds and their descriptors
-    # pageNo = 1
-    # sndCnt = 0
-    # indCnt = 0
-    # totalSnds = min(results.count, 200)  # System quits after trying to download after 200 times
-    #
-    # downloadedSounds = []
-    # while (1):
-    #     if indCnt >= totalSnds:
-    #         print(
-    #             "Not able to download required number of sounds. Either there are not enough search results on freesound for your search query and filtering constraints or something is wrong with this script.")
-    #         break
-    #     sound = results[indCnt - ((pageNo - 1) * page_size)]
-    #     print("Downloading mp3 preview and descriptors for sound with id: %s" % str(sound.id))
-    #     outDir1 = os.path.join(directory, query, str(sound.id))
-    #
-    #     mp3Path = os.path.join(outDir1, str(sound.previews.preview_lq_mp3.split("/")[-1]))
-    #     ftrPath = mp3Path.replace('.mp3', featureExt)
-    #
-    #
-    #     fs.FSRequest.retrieve(sound.previews.preview_lq_mp3, client, mp3Path)
-    #     # Initialize a dictionary to store descriptors
-    #     features = {}
-    #     # Obtaining all the descriptors
-    #     for desc in descriptors:
-    #         features[desc] = []
-    #         features[desc].append(eval("sound.analysis." + desc))
-    #
-    #     # Once we have all the descriptors, store them in a json file
-    #     json.dump(features, open(ftrPath, 'w'))
-    #     sndCnt += 1
-    #     downloadedSounds.append([str(sound.id), sound.url])
-    #
-    #     indCnt += 1
-    #
-    #     if indCnt % page_size == 0:
-    #         results = results.next_page()
-    #         pageNo += 1
-    #
-    #     if sndCnt >= topNresults:
-    #         break
-    #
-    #     # Dump the list of files and Freesound links
-    #     fid = open(os.path.join(outDir2, query + '_SoundList.txt'), 'w')
-    #     for elem in downloadedSounds:
-    #         fid.write('\t'.join(elem) + '\n')
-    #     fid.close()
+    if(not bool(os.listdir(dir_path))):
+
+        print(os.listdir(dir_path))
+
+        # Creating directories to store output and downloading sounds and their descriptors
+        while (1):
+            if indCnt >= totalSnds:
+                print(
+                    "Not able to download required number of sounds. Either there are not enough search results on freesound for your search query and filtering constraints or something is wrong with this script.")
+                break
+            sound = results[indCnt - ((pageNo - 1) * page_size)]
+            print("Downloading mp3 preview and descriptors for sound with id: %s" % str(sound.id))
+
+            mp3Path = os.path.join(dir_path, str(sound.previews.preview_lq_mp3.split("/")[-1]))
+            ftrPath = mp3Path.replace('.mp3', featureExt)
+
+            try:
+                fs.FSRequest.retrieve(sound.previews.preview_lq_mp3, client, mp3Path)
+                # Initialize a dictionary to store descriptors
+                features = {}
+                # Obtaining all the descriptors
+                #for desc in descriptors:
+                #    features[desc] = []
+                #    features[desc].append(eval("sound.analysis." + desc))
+
+                # Once we have all the descriptors, store them in a json file
+                json.dump(features, open(ftrPath, 'w'))
+                sndCnt += 1
+                downloadedSounds.append([str(sound.id), sound.url])
+
+            except:
+                if os.path.exists(dir_path):
+                    print("Bad")
+                    #shutil.rmtree("datos")
+                    #os.system("rm -r " + outDir1)
+
+            indCnt += 1
+
+            if indCnt % page_size == 0:
+                results = results.next_page()
+                pageNo += 1
+
+            if sndCnt >= topNresults:
+                break
+
+        # Dump the list of files and Freesound links
+        fid = open(os.path.join(dir_path, query + '_SoundList.txt'), 'w')
+        for elem in downloadedSounds:
+            fid.write('\t'.join(elem) + '\n')
+        fid.close()
+    else:
+        print("Lleno")
--- AHSC/src/main.py
+++ AHSC/src/main.py
@@ -4,5 +4,8 @@ import sys
 import download as d
 sys.path.append('./freesound-python-master/')
 
+apiCristina = "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR"
+
 print(os.getcwd())
-d.download_sounds_freesound("trumpet", "./src/testDownload", "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR", 5, '.json')
+#d.download_sounds_freesound("trumpet", None, None, "./src/testDownload", "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR", 10, '.json')
+d.download_sounds_freesound("violin", None, None, "./src/testDownload", apiCristina, 5, '.json')
