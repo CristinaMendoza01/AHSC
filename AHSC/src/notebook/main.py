28dfc927a646a4e3f27057591b24e1af4185cabd
cmendoza <72572198+CristinaMendoza01@users.noreply.github.com>
2023-05-05 12:15:13 +0200

    Download Voice sounds from Freesound

--- AHSC/src/notebook/main.py
+++ AHSC/src/notebook/main.py
@@ -12,26 +12,40 @@ apiCristina = "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR"
 # print(os.getcwd())
 
 # Part 1
-print("Part 1: \n")
-d.download_sounds_freesound(query="trumpet", tag="single-note", duration=10, directory="./src/notebook/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
-d.download_sounds_freesound(query="violin", tag="single-note", duration=10, directory="./src/notebook/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
-d.download_sounds_freesound(query="flute", tag="single-note", duration=10, directory="./src/notebook/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
+# print("Part 1: \n")
+# d.download_sounds_freesound(query="trumpet", tag="single-note", duration=10, directory="./src/notebook/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
+# d.download_sounds_freesound(query="violin", tag="single-note", duration=10, directory="./src/notebook/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
+# d.download_sounds_freesound(query="flute", tag="single-note", duration=10, directory="./src/notebook/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
 
 # Part 2
-print("\nPart 2: \n")
-c.clustering()
+# print("\nPart 2: \n")
+# c.clustering()
 
 # Part 3
-print("\nPart 3: \n")
-c.cluster_sounds("./src/notebook/testDownload", nCluster=-1, descInput=[0, 12])
+# print("\nPart 3: \n")
+# c.cluster_sounds("./src/notebook/testDownload", nCluster=-1, descInput=[0, 12])
 
 # Part 4
-print("\nPart 4.1: \n")
-d.download_sounds_freesound(query="guitar", tag="single-note", duration=10, directory="./src/notebook/testClass", api_key=apiCristina, topNresults=15, featureExt='.json')
-d.download_sounds_freesound(query="piano", tag="single-note", duration=10, directory="./src/notebook/testClass", api_key=apiCristina, topNresults=15, featureExt='.json')
+# print("\nPart 4.1: \n")
+# d.download_sounds_freesound(query="guitar", tag="single-note", duration=10, directory="./src/notebook/testClass", api_key=apiCristina, topNresults=15, featureExt='.json')
+# d.download_sounds_freesound(query="piano", tag="single-note", duration=10, directory="./src/notebook/testClass", api_key=apiCristina, topNresults=15, featureExt='.json')
 
 # Funciona la función pero en vez de decir una clase (flute, violin o trumpet), da el nombre de la carpeta
-print("\nPart 4.2: \n")
-cl.classify_sound_kNN("./src/notebook/testClass/guitar/91199/91199_1075352-lq.json", "./src/notebook/testDownload/", 5, descInput=[1, 12])
-cl.classify_sound_kNN("./src/notebook/testClass/guitar/110455/110455_1075352-lq.json", "./src/notebook/testDownload/", 3, descInput=[2, 10])
-cl.classify_sound_kNN("./src/notebook/testClass/piano/673720/673720_8981843-lq.json", "./src/notebook/testDownload/", 3, descInput = [0, 1, 14, 15])
+#print("\nPart 4.2: \n")
+#cl.classify_sound_kNN("./src/notebook/testClass/guitar/91199/91199_1075352-lq.json", "./src/notebook/testDownload/", 5, descInput=[1, 12])
+#cl.classify_sound_kNN("./src/notebook/testClass/guitar/110455/110455_1075352-lq.json", "./src/notebook/testDownload/", 3, descInput=[2, 10])
+#cl.classify_sound_kNN("./src/notebook/testClass/piano/673720/673720_8981843-lq.json", "./src/notebook/testDownload/", 3, descInput = [0, 1, 14, 15])
+
+# Download Voice Sounds
+#d.download_sounds_freesound(query="scream", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+
+#d.download_sounds_freesound(query="whisper", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+
+#d.download_sounds_freesound(query="monster", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+#d.download_sounds_freesound(query="ghost", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+
+#d.download_sounds_freesound(query="breathe", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+# d.download_sounds_freesound(query="breathing", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+
+#d.download_sounds_freesound(query="laugh", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
+#d.download_sounds_freesound(query="laughter", tag=None, duration=None, directory="C:/Users/cris/downloads/voices", api_key=apiCristina, topNresults=500, featureExt='.json')
