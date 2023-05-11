dab1c4c5c1ff20351f330be62b7b801b6fc373da
cmendoza <72572198+CristinaMendoza01@users.noreply.github.com>
2023-05-11 13:06:43 +0200

    Update soundData.py

--- AHSC/src/searchSounds/soundData.py
+++ AHSC/src/searchSounds/soundData.py
@@ -6,3 +6,7 @@ dataset.validate()  # validate that all the expected files are there
 
 example_clip = dataset.choice_clip()  # choose a random example clip
 print(example_clip)  # see the available data
+
+# python 3.8
+# numpy 1.20.0
+# pip install typing-extensions
