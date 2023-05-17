a9ee31a9e767f7752894ea524ef7f31d6b101822
cmendoza <72572198+CristinaMendoza01@users.noreply.github.com>
2023-05-17 14:16:31 +0200

    Organization Update + Upload Scream sounds

--- src/program/datasetCreation.py
+++ src/program/datasetCreation.py
@@ -18,9 +18,11 @@ apiCristina = "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR"
 # Part 1
 #print("Part 1: Download the training sounds from Freesound\n")
 
+############################## TRAINING SOUNDS ######################################
 ######################### CRISTINA SOUNDS ##################################
-#d.download_sounds_freesound(query="scream", tag="human", duration=10, directory="trainingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
-#d.download_sounds_freesound(query="breathe", tag="human", duration=10, directory="trainingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
+#d.download_sounds_freesound(query="scream", tag="voice", duration=10, directory="trainingSet", api_key=apiCristina, topNresults=50, featureExt='.json')
+#d.download_sounds_freesound(query="scream", tag="horror", duration=10, directory="trainingSet", api_key=apiCristina, topNresults=50, featureExt='.json')
+
 ########################### SARA SOUNDS ####################################
 
 ########################## NAIARA SOUNDS ###################################
@@ -35,6 +37,7 @@ apiCristina = "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR"
 #d.download_sounds_freesound(query="guitar", tag="distorted", duration=10, directory="trainingSet", api_key=apiCristina, topNresults=50, featureExt='.json')
 #d.download_sounds_freesound(query="guitar", tag=None, duration=5, directory="trainingSet", api_key=apiCristina, topNresults=30, featureExt='.json')
 #d.download_sounds_freesound(query="piano", tag=None, duration=5, directory="trainingSet", api_key=apiCristina, topNresults=30, featureExt='.json')
+
 ########################## VÍCTOR SOUNDS ###################################
 #d.download_sounds_freesound(query="door knocks", tag=None, duration=3, directory="trainingSet", api_key=apiCristina, topNresults=20, featureExt='.json')
 ############################################################################
@@ -42,12 +45,12 @@ apiCristina = "dCvdtqvTd7A4WhnDF3qSkTZafXKxVqQIxlCM85KR"
 #d.download_sounds_freesound(query="flute", tag="single-note", duration=10, directory="./src/program/testDownload", api_key=apiCristina, topNresults=15, featureExt='.json')
 
 # Part 2
-print("\nPart 2: Do clustering 1 \n")
-#c.clustering()
+#print("\nPart 2: Do clustering 1 \n")
+# c.clustering()
 
 # Part 3
 print("\nPart 3: Do clustering 2\n")
-#c.cluster_sounds("trainingSet", nCluster=-1, descInput=[0, 12])
+c.cluster_sounds("trainingSet", nCluster=-1, descInput=[0, 12])
 
 # Part 4
 #print("\nPart 4.1: Download the test sounds from Freesound \n")
@@ -55,23 +58,26 @@ print("\nPart 3: Do clustering 2\n")
 ############################## TESTING SOUNDS ######################################
 ######################### CRISTINA #################################
 #d.download_sounds_freesound(query="screaming", tag="human", duration=10, directory="testingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
+
 ########################### SARA ###################################
 #d.download_sounds_freesound(query="car engine", tag=None, duration=30, directory="testingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
+
 ########################## NAIARA ##################################
 #d.download_sounds_freesound(query="rain", tag=None, duration=120, directory="testingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
+
 ########################### ARNAU ##################################
 #d.download_sounds_freesound(query="raven bird", tag=None, duration=530, directory="testingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
+
 ########################### LAIA ###################################
 #d.download_sounds_freesound(query="violin", tag=None, duration=50, directory="testingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
+
 ########################## VÍCTOR ##################################
 #d.download_sounds_freesound(query="knocking", tag="knock", duration=10, directory="testingSet", api_key=apiCristina, topNresults=10, featureExt='.json')
 #####################################################################################
 
-
 ############################### CORPUS SOUNDS #######################################
-d.download_sounds_freesound(query="horror sounds", tag=None, duration=10, directory="testingSet", api_key=apiCristina, topNresults=20, featureExt='.json')
+#d.download_sounds_freesound(query="horror sounds", tag=None, duration=10, directory="testingSet", api_key=apiCristina, topNresults=20, featureExt='.json')
 #####################################################################################
-
 
 #d.download_sounds_freesound(query="screaming", tag=None, duration=10, directory="testingSet", api_key=apiCristina, topNresults=1, featureExt='.json')
 # d.download_sounds_freesound(query="piano", tag="single-note", duration=10, directory="./src/program/testClass", api_key=apiCristina, topNresults=15, featureExt='.json')
