f964d2096862ecea00c7ccf1ff21e1957e28a32c
cmendoza <72572198+CristinaMendoza01@users.noreply.github.com>
2023-06-04 22:04:06 +0200

    logo + interface update

--- src/interface/mainGUI.py
+++ src/interface/mainGUI.py
@@ -2,6 +2,10 @@ import shutil
 import tkinter as tk
 import wave
 import webbrowser
+from PIL import ImageTk, Image
+from io import BytesIO
+
+import requests
 
 import soundfile
 
@@ -11,6 +15,10 @@ from soundImprover import *
 
 import simpleaudio as sa
 
+bg1 = "#BA93D6" # Principal
+bg2 = "#CDB3DF" # Screen
+bg3 = "#B283D4" # Container
+
 class Interface(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -19,60 +27,76 @@ class Interface(tk.Tk):
         self.geometry("1000x600")
 
         self.top_frame = tk.Frame(self)
-        self.top_frame.configure(bg="#9D7CC5")
+        self.top_frame.configure(bg=bg1)
         self.top_frame.pack(side=tk.TOP, fill=tk.X)
 
         self.down_frame = tk.Frame(self)
-        self.down_frame.configure(bg="#9D7CC5")
+        # self.down_frame.configure(bg="#9D7CC5")
+        self.down_frame.configure(bg=bg1)
         self.down_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
-        self.title_label = tk.Label(self.top_frame, text="AHSC: Automatic Horror Sound Classification", font=("Arial", 15), fg="black", bg="#9D7CC5")
-        self.title_label.pack(pady=20)
+        self.title_label = tk.Label(self.top_frame, text="AHSC: Automatic Horror Sound Classification", font=("Arial", 15), fg="black", bg=bg1)
+        # self.title_label.pack(pady=20)
+        self.title_label.configure(justify="center")
+        self.title_label.grid(row=0, column=1, padx=150)
 
         self.main_frame = tk.Frame(self)
         self.main_frame.pack(expand=True, fill=tk.BOTH)
 
         self.button_frame = tk.Frame(self.main_frame, width=100)
-        self.button_frame.configure(bg="#9D7CC5")
+        self.button_frame.configure(bg=bg1)
         self.button_frame.pack(side=tk.LEFT, fill=tk.Y)
 
         self.screen_frame = tk.Frame(self.main_frame)
-        self.screen_frame.configure(bg="#BE96EE")
+        self.screen_frame.configure(bg=bg2)
         self.screen_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 
-        self.category_label = tk.Label(self.button_frame, text="Categories:", font=("Arial", 15), fg="black", bg="#9D7CC5")
+        self.category_label = tk.Label(self.button_frame, text="Categories:", font=("Arial", 15), fg="black", bg=bg1)
         self.category_label.pack(pady=10, padx=20)
 
-        self.login_label = tk.Label(self.top_frame, text="Login", font=("Arial", 10))
-        self.login_label.pack(pady=10)
+        self.login_label = tk.Label(self.top_frame, text="Login", fg="black", font=("Arial", 10))
+        # self.login_label.pack(pady=10)
+        self.login_label.grid(row=0, column=2, pady=10)
         # Configuring the label as a link
-        self.login_label.config(background="#9D7CC5", cursor="hand2")
+        self.login_label.configure(background=bg1, cursor="hand2", justify="center")
         self.login_label.bind("<Button-1>", show_login)
+
+        ################################### LOGO ###############################################
+        # Load the image
+        self.image = Image.open("C:/Users/cris/Downloads/logo.png")  # Replace with the path to your image
+        self.image = self.image.resize((80, 80))  # Resize the image as desired
+        self.photo = ImageTk.PhotoImage(self.image)
+
+        # Create a label to display the image
+        self.image_label = tk.Label(self.top_frame, image=self.photo)
+        self.image_label.grid(row=0, column=0, sticky="nw", padx=30, pady=10)
+        # self.image_label.pack()
+        #########################################################################################
 
         ################################### BUTTONS ###############################################
         self.aboutButton = tk.Button(self.down_frame, text="About", font=("Arial", 10), command=self.show_about,
-                                     background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+                                     background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.aboutButton.pack(side=tk.TOP, pady=5)
 
-        self.button1 = tk.Button(self.button_frame, text="Screaming", command=self.show_screen1, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button1 = tk.Button(self.button_frame, text="Screaming", command=self.show_screen1, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button1.pack()
 
-        self.button2 = tk.Button(self.button_frame, text="Car Engine", command=self.show_screen2, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button2 = tk.Button(self.button_frame, text="Car Engine", command=self.show_screen2, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button2.pack()
 
-        self.button3 = tk.Button(self.button_frame, text="Owl", command=self.show_screen3, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button3 = tk.Button(self.button_frame, text="Owl", command=self.show_screen3, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button3.pack()
 
-        self.button4 = tk.Button(self.button_frame, text="Knocking", command=self.show_screen4, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button4 = tk.Button(self.button_frame, text="Knocking", command=self.show_screen4, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button4.pack()
 
-        self.button5 = tk.Button(self.button_frame, text="Rain", command=self.show_screen5, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button5 = tk.Button(self.button_frame, text="Rain", command=self.show_screen5, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button5.pack()
 
-        self.button6 = tk.Button(self.button_frame, text="Raven Bird", command=self.show_screen6, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button6 = tk.Button(self.button_frame, text="Raven Bird", command=self.show_screen6, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button6.pack()
 
-        self.button7 = tk.Button(self.button_frame, text="Violin", command=self.show_screen7, background="#9D7CC5", relief=tk.FLAT, borderwidth=0)
+        self.button7 = tk.Button(self.button_frame, text="Violin", command=self.show_screen7, background=bg1, relief=tk.FLAT, borderwidth=0, fg="black")
         self.button7.pack()
         #########################################################################################
 
@@ -186,10 +210,10 @@ def downloadSound(sound):
 class Screen(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.configure(bg="#BE96EE")
+        self.configure(bg=bg2)
 
         # Create a canvas widget
-        canvas = tk.Canvas(self, bg="#BE96EE")
+        canvas = tk.Canvas(self, bg=bg2)
         canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         # Create a scrollbar and associate it with the canvas
@@ -198,9 +222,14 @@ class Screen(tk.Frame):
         canvas.configure(yscrollcommand=scrollbar.set)
 
         # Create a frame inside the canvas to hold the content
-        inner_frame = tk.Frame(canvas, bg="#BE96EE")
+        inner_frame = tk.Frame(canvas, bg=bg2)
         inner_frame.pack()
         canvas.create_window((0, 0), window=inner_frame, anchor=tk.NW)
+
+        # Create a frame inside the canvas to hold the about content
+        about_frame = tk.Frame(canvas, bg=bg2)
+        about_frame.pack()
+        canvas.create_window((0, 0), window=about_frame, anchor=tk.NW)
 
         # Create a horizontal scroll bar
         scrollbar = tk.Scrollbar(canvas, orient="horizontal", command=canvas.xview)
@@ -209,13 +238,13 @@ class Screen(tk.Frame):
 
         sounds = []
         if(interface.count == 0):       # About Screen
-            label = tk.Label(inner_frame, text="This project is called AHSC: Automatic Horror Sound Classification and it's about to create a software that classifies different horror sounds. \nBasically, we get horror sounds from Freesound (https://freesound.org/) and we create a software application. \nThis application classifies these sounds into different categories. \n\nAHSC is an innovative project developed as part of the Music Technology Workshop elective course for ICT engineering degrees at UPF. \nThe aim of this project is to develop a software application based on the 'Freesound' website to classify horror sounds by using a new \ntagging system created by us to classify these horror sounds automatically in subcategories. \n\nWith almost 12000 available horror-related sounds on the website without specific tags, \nthis project proposes a solution that involves using metadata and implementing classification algorithms to generate subcategories, \nwhich will allow for more accurate and specific searches. \n\nTo achieve this, the project employs the Python programming language and the Pycharm IDE, along with the powerful 'Essentia' \nlibraries that help with sound analysis and descriptor extraction, and the 'Scikit-learn' library for machine learning techniques.")
-            label.configure(font=("Arial", 10), background="#BE96EE")
-            label.pack(anchor='w')
+            label = tk.Label(about_frame, text="This project is called AHSC: Automatic Horror Sound Classification and it's about to create a software that classifies different horror sounds. \nBasically, we get horror sounds from Freesound (https://freesound.org/) and we create a software application. \nThis application classifies these sounds into different categories. \n\nAHSC is an innovative project developed as part of the Music Technology Workshop elective course for ICT engineering degrees at UPF. \nThe aim of this project is to develop a software application based on the 'Freesound' website to classify horror sounds by using a new \ntagging system created by us to classify these horror sounds automatically in subcategories. \n\nWith almost 12000 available horror-related sounds on the website without specific tags, \nthis project proposes a solution that involves using metadata and implementing classification algorithms to generate subcategories, \nwhich will allow for more accurate and specific searches. \n\nTo achieve this, the project employs the Python programming language and the Pycharm IDE, along with the powerful 'Essentia' \nlibraries that help with sound analysis and descriptor extraction, and the 'Scikit-learn' library for machine learning techniques.")
+            label.configure(font=("Arial", 10), background=bg2, fg="black")
+            label.pack(anchor='w', pady=10, padx=10)
 
-            label2 = tk.Label(inner_frame, text="Víctor Alcaide \nSara Barbé \nLaia Duch \nNaiara Garmendia \nArnau Adan \nCristina Mendoza")
-            label2.configure(font=("Arial", 10), background="#BE96EE")
-            label2.pack()
+            label2 = tk.Label(about_frame, text="Víctor Alcaide \nSara Barbé \nLaia Duch \nNaiara Garmendia \nArnau Adan \nCristina Mendoza")
+            label2.configure(font=("Arial", 10), background=bg2, fg="black")
+            label2.pack(pady=10, padx=10)
 
         elif(interface.count == 1):     # Screaming Screen
             sounds = obtainInfoSounds(screamingDir)
@@ -237,31 +266,46 @@ class Screen(tk.Frame):
             response = retrieveSound(sound)
             # print(response)
             container = tk.Frame(inner_frame)
+            container.configure(bg=bg3)
             container.pack(pady=20, padx=10, fill=tk.X)
 
+            # Load the image
+            image_url = response['images']['waveform_m']
+            image_response = requests.get(image_url)
+            image = Image.open(BytesIO(image_response.content))
+            photo = ImageTk.PhotoImage(image)
+
+            # Create a label to display the image
+            image_label = tk.Label(container, image=photo)
+            image_label.grid(row=0, column=0, sticky="nw", padx=10, pady=20)
+
             name_label = tk.Label(container, text=response['name'] + ' - ' + response['username'])
-            name_label.grid(row=0, column=0, padx=10, pady=20)
+            name_label.configure(fg="black", background=bg3)
+            name_label.grid(row=0, column=1, padx=10, pady=20)
 
             id_label = tk.Label(container, text=str(response['id']))
-            id_label.grid(row=0, column=1, padx=10, pady=20)
+            id_label.configure(fg="black", background=bg3)
+            id_label.grid(row=0, column=2, padx=10, pady=20)
 
             play_label = tk.Label(container, text="Play")
-            play_label.grid(row=0, column=2, padx=10, pady=20)
-            play_label.config(cursor="hand2")
+            play_label.grid(row=0, column=3, padx=10, pady=20)
+            play_label.config(cursor="hand2", fg="black", background=bg3)
             play_label.bind("<Button-1>", lambda event, sound=response: playSound(sound))
 
             download_label = tk.Label(container, text="Download")
-            download_label.grid(row=0, column=3, padx=10, pady=20)
-            download_label.config( cursor="hand2")
+            download_label.grid(row=0, column=4, padx=10, pady=20)
+            download_label.config( cursor="hand2", fg="black", background=bg3)
             download_label.bind("<Button-1>", lambda event, sound=response: downloadSound(sound))
 
             i = 1
             for tag in response['tags']:
                 tags_label = tk.Label(container, text=tag)
+                tags_label.configure(fg="black", background=bg3)
                 tags_label.grid(row=1, column=i, pady=20, padx=10)
                 i += 1
 
             license_label = tk.Label(container, text="License: " + response['license'])
+            license_label.configure(fg="black", background=bg3)
             license_label.grid(row=1, column=0, padx=10, pady=20)
 
         # Configure the canvas to scroll the inner frame
